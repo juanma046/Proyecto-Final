7360ce6bbf8c3bdf8212a71f77f537e09f749b9b
Juanma <jsangam046a@ieslacampina.es>
2024-03-21 10:49:26 +0100

    Completando Registrar1

--- registrar1.php
+++ registrar1.php
@@ -12,14 +12,19 @@
     <title>Registrar Participante</title>
 </head>
 <body>
-    <p>Introduce el nombre del participante<input type="text" name="nombre"></p>
-    <p><label for="formControlInput" class="form-label">Selecciona su pokemon</label>
-							<select class="form-control" id="formControlInput" name="id_pokemon">
-								<option value="1">Bloodborne</option>
-								<option value="2">Fortnite</option>
-                                <option value="3">Persona 3 Reload</option>
-								<option value="4">F1 2024</option>
-							</select></p>
-    <p>Introduce el nivel del pokémon: <input type="number" name="nivel" min=1 max=100></p>
+    <form action="registrar2.php" method="post">
+    <p>Introduce el nombre del participante:<input type="text" name="nombre" required></p>
+    <p>Selecciona el pokémon:
+    <?php 
+        echo "<select name='pokemon'>";
+        while($fila = $resultado->fetch_assoc()){
+            echo "<option value='$fila[id_pokemon]'>$fila[Nombre]</option>";
+        }
+        echo "</select>";
+    ?>
+    </p>
+    <p>Introduce el nivel del pokémon: <input type="number" name="nivel" min=1 max=100 required></p>
+    <p><input type="submit" value="Registrar" name="submit"></p>
+    </form>
 </body>
 </html>
