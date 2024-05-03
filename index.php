836b5003ddb9913f767ed76afdabfdd5ae5f4573
juanma046 <jsangam046a@ieslacampina.es>
2024-05-03 12:59:50 +0200

    Index Clasificación

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
     require 'conexion.php';
 
-    $sql1 = "SELECT * FROM participantes";
+    $sql1 = "SELECT * FROM participantes ORDER BY Ganadas DESC";
 
     $resultado1 = $mysqli->query($sql1);
+    //$fila = $resultado1->fetch_assoc();
+    //$ganadas=$fila['Ganadas'];
+    //$jugadas=$fila['Jugadas'];
+    //if ($ganadas==0){
+    //    $media=0;
+    //}else{
+    //    $media=($jugadas/$ganadas)*100;
+    //}
 ?>
 
 <!DOCTYPE html>
@@ -20,22 +28,26 @@
         echo "<table id='tabla'>";
         echo "<tr>";
             echo "<th>Nombre</th>";
+            echo "<th>Pokémon</th>";
             echo "<th>Jugadas</th>";
             echo "<th>Ganadas</th>";
+            echo "<th>Porcentaje de Victorias</th>";
         echo "</tr>";
         
         while($fila1 = $resultado1->fetch_assoc()){
             echo "<tr>";
                 echo "<td>$fila1[Nombre]</td>";
+                echo "<td>$fila1[individuo_pokemon]</td>";
                 echo "<td>$fila1[Jugadas]</td>";
                 echo "<td>$fila1[Ganadas]</td>";
-                echo "<td><button class='btn btn-warning'><a href='modificar.php?id=$fila1[id_participante]' class='text-white'>Editar</a></button></td>";
+                //echo "<td>$media%</td>";
+                echo "<td><button class='btn btn-warning'><a href='modificar.php?id=$fila1[id_participante]' class='text-white'>Cambiar pokémon</a></button></td>";
                 echo "<td><button class='btn btn-danger'><a href='eliminar.php?id=$fila1[id_participante]' class='text-white'>Eliminar</a></button></td>";
             echo "</tr>";
         }
         echo "</table>";
         
-            $mysqli->close();
+        $mysqli->close();
     ?>
     <p><a href="registrar1.php">Registrar nuevo participante</a></p>
 </body>
