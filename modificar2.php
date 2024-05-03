7bea2f9b671ac20049942235efc8afc02260bed9
juanma046 <jsangam046a@ieslacampina.es>
2024-05-03 12:58:57 +0200

    Modificar 2

---
+++ modificar2.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		
+		<title>Torneo Pokémon</title>
+	</head>
+	<body>
+		<?php
+			//Establezco conexión
+			require 'conexion.php';
+
+			//Obtengo los datos introducidos en el formulario anterior
+			$id = $_POST["id"];
+			$ID_pokemon = $_POST["pokemon"];
+
+            $sqlpoke = "SELECT Nombre FROM pokémon WHERE id_pokemon LIKE '$ID_pokemon'";
+            $resultadoP = $mysqli->query($sqlpoke);
+
+            $fila = $resultadoP->fetch_assoc();
+            $nombre_pokemon = $fila['Nombre'];
+	
+			//Se repara la sentencia SQL
+			$sql = "UPDATE participantes SET individuo_pokemon='$nombre_pokemon' WHERE id_participante=$id";
+			//Se ejecuta la sentencia y se guarda el resultado en $resultado
+			$resultado = $mysqli->query($sql);
+	
+			if($resultado > 0){
+				echo "<button type='button' class='btn btn-lg btn-primary' disabled>El registro ha sido editado correctamente</button>";
+			   //header("Location: index.php");
+			}else{
+				echo "<p class='bg-danger text-white'>Ha habido un error al modificar un registro</p>";
+			}
+			echo "<br>";
+			echo "<button class='btn btn-primary'><a href='index.php' class='text-white'> Regresar</a></button>";
+		?>
+	</body>
+</html>
