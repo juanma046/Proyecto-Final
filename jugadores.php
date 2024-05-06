ce39c05df15b34dbe7a8d93e02f3b532005f00c2
juanma046 <jsangam046a@ieslacampina.es>
2024-05-06 23:14:46 +0200

    nombre de los pokemons de cada jugador en jugadores.php

--- jugadores.php
+++ jugadores.php
@@ -18,6 +18,12 @@
 
 	//Se extrae el registro. No se hace en bucle porque el resultado deber ser único
     $fila = $resultado->fetch_assoc();
+    $ID_pokemon = $fila['individuo_pokemon'];
+
+    $sqlpoke = "SELECT Nombre FROM pokémon WHERE id_pokemon LIKE '$ID_pokemon'";
+            $resultadoP = $mysqli->query($sqlpoke);
+            $fila_poke = $resultadoP->fetch_assoc();
+            $nombre_pokemon = $fila_poke['Nombre'];
 
         echo "<h1>$fila[Nombre]</h1>";
 
@@ -29,7 +35,7 @@
             echo "<th>Porcentaje de Victorias</th>";
         echo "</tr>";
             echo "<tr>";
-                echo "<td>$fila[individuo_pokemon]</td>";
+                echo "<td>$nombre_pokemon</td>";
                 echo "<td>$fila[Jugadas]</td>";
                 echo "<td>$fila[Ganadas]</td>";
                 echo "<td><button class='btn btn-warning'><a href='modificar.php?id=$fila[id_participante]' class='text-white'>Cambiar pokémon</a></button></td>";
