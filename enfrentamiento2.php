f2558a52b9aeb6d0bed323b7258d373cbde0ed8c
juanma046 <jsangam046a@ieslacampina.es>
2024-05-29 20:55:30 +0200

    agregando bootstrap

--- enfrentamiento2.php
+++ enfrentamiento2.php
@@ -55,6 +55,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pokemon.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -19,18 +19,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="pokemon.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <title>Torneo de Pokémon</title>
 </head>
 <body>
 <div class='contenedor'> <!-- Le damos un tamaño medio de 4 a cada columna --> 
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class='container-fluid'>
-            <img src="imagenes/escudo.svg" class="escudo">
-            <h1>SIMULADOR COMBATES POKÉMON</h1>
-        </div>
-    </nav>
-    </header>
+<header>  
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"><!-- Boton para el menu movil -->
+            <div class="container-fluid"> <!-- Determina el width y el height como 100% -->
+                <img src="imagenes/escudo.svg" class="escudo">
+    
+                
+    
+                
+                <div class="navbar-collapse" id="menu"><!--navbar-collapse es para agrupar el contenido de la barra de navegación pora breakpoint determinado.-->
+                     <!--  navbar permite anclar la barra de navegación a la parte superior o inferior de la pantalla y que siempre sea visible -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0"><!--me-auto es para centrar horizontalmente contenido de nivel de bloque de ancho fijo, es decir, contenido que tiene display: block y un conjunto width , configurando los márgenes horizontales en auto -->
+                        <li class="nav-item"><a href="#" class="nav-link">Clasificación</a></li> <!-- Utilizamos la clase nav item para agregarlo al menú del navbar -->
+                        <li class="nav-item"><a href="pokemon.php" class="nav-link">Lista de Pokemons</a></li>
+                        <li class="nav-item"><a href="enfrentamiento1.php" class="nav-link">Simulador de Combate</a></li>
+                        <li class="nav-item"><a href="https://www.youtube.com/watch?v=puc-HK3atGQ&ab_channel=MiqueCaleb" class="nav-link">Historia</a></li>
+                        <li class="nav-item dropdown">
+                            <a 
+                                href="#" 
+                                class="nav-link dropdown-toggle"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                                id="dropdown-menu"
+                            >
+                                Items
+                            </a>
+    
+                            <ul class="dropdown-menu" aria-labelledby="dropdown-menu">
+                                <li><a href="https://darksouls.fandom.com/es/wiki/Armas_de_Dark_Souls" class="dropdown-item"><img src="imagenes/espada.jpg" width="50px">Armas</a></li>
+                                <li><a href="https://darksouls.fandom.com/es/wiki/Escudos_de_Dark_Souls" class="dropdown-item"><img src="imagenes/escudo.jpg" width="50px">Escudos</a></li>
+                                <li><a href="https://darksouls.fandom.com/es/wiki/Armaduras_de_Dark_Souls" class="dropdown-item"><img src="imagenes/armadura.jpg" width="50px">Armaduras</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
     <main>
 
     <div class="video">
