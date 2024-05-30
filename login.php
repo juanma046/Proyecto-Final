00cdf2ed8f5561e1a6f3679a97bdac40fd0f7943
juanma046 <jsangam046a@ieslacampina.es>
2024-05-30 23:17:20 +0200

    Agregando Inicio de Sesión

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <form action="loguear.php" method="POST">
+            <input type="text" name="usuario">
+            <br><br>
+            <input type="password" name="clave">
+            <br><br>
+            <input type="submit" value="Iniciar Sesión">
+        </form>
+    </center>
+</body>
+</html>
