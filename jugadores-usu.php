6c3a605eb8ee9d55513ca6194f9e3040ea4fc29c
juanma046 <jsangam046a@ieslacampina.es>
2024-05-31 19:19:07 +0200

    agregando menú de navegación

--- jugadores-usu.php
+++ jugadores-usu.php
@@ -11,6 +11,38 @@
     <title>Jugadores</title>
 </head>
 <body>
+<div class='contenedor'> <!-- Le damos un tamaño medio de 4 a cada columna --> 
+<header>  
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"><!-- Boton para el menu movil -->
+            <div class="container-fluid"> <!-- Determina el width y el height como 100% -->
+                <img src="imagenes/escudo.svg" class="escudo">
+                <div class="navbar-collapse" id="menu"><!--navbar-collapse es para agrupar el contenido de la barra de navegación pora breakpoint determinado.-->
+                     <!--  navbar permite anclar la barra de navegación a la parte superior o inferior de la pantalla y que siempre sea visible -->
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0"><!--me-auto es para centrar horizontalmente contenido de nivel de bloque de ancho fijo, es decir, contenido que tiene display: block y un conjunto width , configurando los márgenes horizontales en auto -->
+                        <li class="nav-item"><a href="pokemon.php" class="nav-link">Lista de Pokemons</a></li>
+                        <li class="nav-item"><a href="enfrentamiento1.php" class="nav-link">Simulador de Combate</a></li>
+                        <li class="nav-item"><a href="salir.php" class="nav-link">Cerrar sesión</a></li>
+                        <li class="nav-item dropdown">
+                            <a 
+                                href="#" 
+                                class="nav-link dropdown-toggle"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                                id="dropdown-menu"
+                            >
+                                Items
+                            </a>
+    
+                            <ul class="dropdown-menu" aria-labelledby="dropdown-menu">
+                                <li><a href="https://darksouls.fandom.com/es/wiki/Armas_de_Dark_Souls" class="dropdown-item"><img src="imagenes/espada.jpg" width="50px">Armas</a></li>
+                                <li><a href="https://darksouls.fandom.com/es/wiki/Escudos_de_Dark_Souls" class="dropdown-item"><img src="imagenes/escudo.jpg" width="50px">Escudos</a></li>
+                                <li><a href="https://darksouls.fandom.com/es/wiki/Armaduras_de_Dark_Souls" class="dropdown-item"><img src="imagenes/armadura.jpg" width="50px">Armaduras</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
 <?php
     //Recogemos el id del participante
     $id = $_GET['id'];
@@ -74,6 +106,7 @@
     $mysqli->close();
     }
     ?>
+</div>
 </body>
 <script src="bootstrap.bundle.min.js"></script>
 </html>
